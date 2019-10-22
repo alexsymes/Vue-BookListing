a93082371b4070d8a1f3d9842a58ac514692ec75
AlexSymes <asymes@nanthealth.com>
2019-10-22 13:14:00 +0100

    Part3 correction 2

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -4,12 +4,8 @@
 
 <script>
     export default{
-        name: {
-            BookItem
-        }
-        props: [
-            'book'
-        ]
+        name: 'BookItem',
+        props: ['book']
     }
 </script>
 
